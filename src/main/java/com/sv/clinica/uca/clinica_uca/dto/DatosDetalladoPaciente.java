de32600d8727f06e21d9ad88f52c7632de89ba55
Sirenoman <00003120@uca.edu.sv>
2023-11-23 14:45:12 -0600

    :art: cambio de detallePaciente

--- src/main/java/com/sv/clinica/uca/clinica_uca/dto/DatosDetalladoPaciente.java
+++ src/main/java/com/sv/clinica/uca/clinica_uca/dto/DatosDetalladoPaciente.java
@@ -3,13 +3,14 @@ package com.sv.clinica.uca.clinica_uca.dto;
 import com.sv.clinica.uca.clinica_uca.model.Paciente;
 
 public record DatosDetalladoPaciente(
+		Long id,
 		String nombre,
 		String apellido,
 		String documentoId,
 		String telefono) {
 	
 	public DatosDetalladoPaciente(Paciente paciente) {
-		this(paciente.getNombre(), paciente.getApellido(), paciente.getDocumentoId(),
+		this(paciente.getId(),  paciente.getNombre(), paciente.getApellido(), paciente.getDocumentoId(),
 				 paciente.getTelefono());
 	}
 
