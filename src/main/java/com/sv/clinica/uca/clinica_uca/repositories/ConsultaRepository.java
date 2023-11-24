673b2b26cd62c53e60048a451729367f6ab6682e
Sirenoman <00003120@uca.edu.sv>
2023-11-24 13:00:09 -0600

    :bug: arreglando problema con consulta

--- src/main/java/com/sv/clinica/uca/clinica_uca/repositories/ConsultaRepository.java
+++ src/main/java/com/sv/clinica/uca/clinica_uca/repositories/ConsultaRepository.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.sv.clinica.uca.clinica_uca.model.Consulta;
@@ -17,7 +18,7 @@ public interface ConsultaRepository extends JpaRepository<Consulta, Long>{
 	Boolean existsByMedicoIdAndFecha(Long idMedico, LocalDateTime fecha);
 	
 	// QUERY Personalizada para buscar todas las consutlas por IdPaciente
-	Page<Consulta> findAllByPacienteIdOrdenByFecha(Long idPaciente, Pageable paginacion);
+	Page<Consulta> findAllByPacienteIdAndOrdenByFecha(Long idPaciente, Pageable paginacion);
 	
 	// QUERY PERSONALIZADA PARA BUSCAR CITA EN ESPECIFICO POR ID PACIENTE
 	//Consulta getReferenceByPacienteId(Long idPaciente);
